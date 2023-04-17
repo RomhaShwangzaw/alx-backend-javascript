65f1df5ec4de4c6f7dff7253529be380322385cf
Romha Shwangzaw <romhas@gmail.com>
2023-04-18 00:16:39 +0300

    Finetuned the function

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let names = '';
-  for (const name of reportWithIterator) {
-    names += `${name} | `;
-  }
-  return names.substring(0, names.length - 3);
+  return reportWithIterator.join(' | ');
 }
