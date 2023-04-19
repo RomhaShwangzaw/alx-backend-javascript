3af83f4dcedeb06ef3cef75d3f9385e12adaf63b
Romha Shwangzaw <romhas@gmail.com>
2023-04-19 15:44:23 +0300

    Lint Fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -16,7 +16,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       } else {
         resArray.push({
           status: result.status,
-          value: `Error: ${val.reason.message}`,
+          value: `Error: ${result.reason.message}`,
         });
       }
     });
