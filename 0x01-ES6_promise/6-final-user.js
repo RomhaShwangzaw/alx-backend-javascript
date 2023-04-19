cf2bfd966ad8ce5af6bf7d4dcc7d62c319dcb1ee
Romha Shwangzaw <romhas@gmail.com>
2023-04-19 15:41:06 +0300

    Bug Fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,21 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const u = signUpUser(firstName, lastName);
   const p = uploadPhoto(fileName);
 
-  return Promise.allSettled([u, p]);
+  return Promise.allSettled([u, p]).then((results) => {
+    const resArray = [];
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        resArray.push({
+          status: result.status,
+          value: result.value,
+        });
+      } else {
+        resArray.push({
+          status: result.status,
+          value: result.reason,
+        });
+      }
+    });
+    return resArray;
+  });
 }
