c9545e4468ebc05df326f74d70641a800d9b6bf9
Romha Shwangzaw <romhas@gmail.com>
2023-04-24 15:02:39 +0300

    added getStudentIdsSum() function

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,7 @@
+export default function getStudentIdsSum(students) {
+  const initialValue = 0;
+  return students.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.id,
+    initialValue,
+  );
+}
